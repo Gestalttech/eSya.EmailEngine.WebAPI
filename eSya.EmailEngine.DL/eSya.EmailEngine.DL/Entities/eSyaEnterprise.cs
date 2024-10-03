aec3d9ea944fd29ea75ec61916efdb684df1c3f3
AbdulR <abdulr@gestalttechindia.in>
2024-10-03 16:39:42 +0530

    GT_ECBSLN table Date format and Short Date format column added

--- eSya.EmailEngine.DL/eSya.EmailEngine.DL/Entities/eSyaEnterprise.cs
+++ eSya.EmailEngine.DL/eSya.EmailEngine.DL/Entities/eSyaEnterprise.cs
@@ -83,6 +83,8 @@ namespace eSya.EmailEngine.DL.Entities
 
                 entity.Property(e => e.CurrencyCode).HasMaxLength(4);
 
+                entity.Property(e => e.DateFormat).HasMaxLength(25);
+
                 entity.Property(e => e.FormId)
                     .HasMaxLength(10)
                     .IsUnicode(false)
@@ -97,6 +99,8 @@ namespace eSya.EmailEngine.DL.Entities
                 entity.Property(e => e.ModifiedOn).HasColumnType("datetime");
 
                 entity.Property(e => e.ModifiedTerminal).HasMaxLength(50);
+
+                entity.Property(e => e.ShortDateFormat).HasMaxLength(15);
 
                 entity.Property(e => e.ShortDesc).HasMaxLength(15);
             });
